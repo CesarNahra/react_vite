aeabaa46aa5fbf798a918d70f46cf0e7cf6207f8
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-07-05 18:24:32 -0300

    Início modal

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
 import './MainComponent.css';
+import './ModalComponent.css';
 
 export const MainComponent = () => {
 
-    // const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-    // const handleShowModal = () => {
-    //     setShowModal(!showModal);
-    // }
+    const handleShowModal = () => {
+        setShowModal(!showModal);
+    }
+
+    const ModalComponent = () => {
+        return (
+            <div className="modal-wrapper">
+                <div className="product-wrapper-modal">
+                    <div className="product-image-wrapper-modal">
+                        <span className="product-price-modal">R$ 1000</span>
+                    </div>
+                    <div className="product-info-wrapper-modal">
+                        <h3 className="product-name-wrapper-modal">Produto 1</h3>
+                        <p className="product-description-wrapper-modal">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <ul className="product-features-wrapper-modal">
+                            <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                            <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                            <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                            <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                        </ul>
+                        <button type='button' className='btn-buy-wrapper-modal' onClick={handleShowModal}>Fechar modal</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     const products = [
         {
@@ -29,23 +54,24 @@ export const MainComponent = () => {
         },
     ]
 
-    
-    return(
+
+    return (
         <div className="main-wrapper">
             <div className="product-wrapper">
                 <div className="product-image-wrapper">
-                    <span className="product-price-wrapper">{products[0].price}</span>
+                    <span className="product-price-wrapper">R$ 1.000</span>
                 </div>
                 <div className="product-info-wrapper">
-                    <h3 className="product-name-wrapper">{products[0].name}</h3>
+                    <h3 className="product-name-wrapper">Produto 1</h3>
                     <p className="product-description-wrapper">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     <ul className="product-features-wrapper">
-                        <li><i class="bi bi-bookmark-fill"></i>Olá Mundo</li>
-                        <li><i class="bi bi-bookmark-fill"></i>Olá Mundo</li>
-                        <li><i class="bi bi-bookmark-fill"></i>Olá Mundo</li>
-                        <li><i class="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                        <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                        <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                        <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
+                        <li><i className="bi bi-bookmark-fill"></i>Olá Mundo</li>
                     </ul>
-                    <button type='button' className="btn-buy-wrapper">Mais sobre</button>
+                    {showModal && <ModalComponent />}
+                    <button type='button' className="btn-buy-wrapper" onClick={handleShowModal}>Mais sobre</button>
                 </div>
             </div>
         </div>
--- src/components/QuestionComponent/QuestionComponent.jsx
+++ src/components/QuestionComponent/QuestionComponent.jsx
@@ -3,34 +3,34 @@ import './QuestionComponent.css';
 export const QuestionComponent = () => {
     return (
         <div className="accordion-wrapper">
-            <div class="accordion accordion-flush" id="accordionFlushExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                             Onde são fabricadas as bicicletas
                         </button>
                     </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                         <div class="accordion-body">As bicicletas são fabricadas na Holanda, em Amsterdã, no nosso polo criativo</div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                             A cobertura da garantia cobre defeitos de fábrica
                         </button>
                     </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Sim, aqui a garantia é total, se a sua bicicleta apresentar defeitos inerentes de fábrica você pode contatar uma de nossas autorizadas ao redor do globo para atendê-lo, se mesmo assim você não se sentir satisfeito nós devolveremos o seu dinheiro de maneira integral, sem qualquer ônus da sua parte. O risco é nosso</div>
+                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Sim, aqui a garantia é total, se a sua bicicleta apresentar defeitos inerentes de fábrica você pode contatar uma de nossas autorizadas ao redor do globo para atendê-lo, se mesmo assim você não se sentir satisfeito nós devolveremos o seu dinheiro de maneira integral, sem qualquer ônus da sua parte. O risco é nosso</div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                             Como andar de bicicleta
                         </button>
                     </h2>
-                    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                         <div class="accordion-body">Clique nesse <a href="https://www.youtube.com/watch?v=DEjf13F4q3k">link</a> para ter acesso a um tutorial prático e didático de como andar de bicicleta e aproveitar a sua jornada sobre duas rodas</div>
                     </div>
                 </div>
